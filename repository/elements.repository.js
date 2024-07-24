14ca2a90bc712d70ee62d6628c3d6fa5a0773e03
Muath-AlBassam <muathalbassam@gmail.com>
2024-07-24 13:25:14 +0300

    create elements repository

---
+++ repository/elements.repository.js
@@ -0,0 +1,105 @@
+const ElementsRepository = {
+    GIElements: new Map([
+        ['Pyro', {
+            name: 'Pyro',
+            imageUrl: 'assets/gi/GI_Pyro.png'
+        }],
+        ['Hydro', {
+            name: 'Hydro',
+            imageUrl: 'assets/gi/GI_Hydro.png'
+        }],
+        ['Dendro', {
+            name: 'Dendro',
+            imageUrl: 'assets/gi/GI_Dendro.png'
+        }],
+        ['Electro', {
+            name: 'Electro',
+            imageUrl: 'assets/gi/GI_Electro.png'
+        }],
+        ['Anemo', {
+            name: 'Anemo',
+            imageUrl: 'assets/gi/GI_Anemo.png'
+        }],
+        ['Cryo', {
+            name: 'Cryo',
+            imageUrl: 'assets/gi/GI_Cryo.png'
+        }],
+        ['Geo', {
+            name: 'Geo',
+            imageUrl: 'assets/gi/GI_Geo.png'
+        }]
+    ]),
+
+    HSRElements: new Map([
+        ['Physical', {
+            name: 'Physical',
+            imageUrl: 'assets/hsr/HSR_Physical.jpg'
+        }],
+        ['Fire', {
+            name: 'Fire',
+            imageUrl: 'assets/hsr/HSR_Fire.jpg'
+        }],
+        ['Ice', {
+            name: 'Ice',
+            imageUrl: 'assets/hsr/HSR_Ice.jpg'
+        }],
+        ['Lightning', {
+            name: 'Lightning',
+            imageUrl: 'assets/hsr/HSR_Lightning.jpg'
+        }],
+        ['Wind', {
+            name: 'Wind',
+            imageUrl: 'assets/hsr/HSR_Wind.jpg'
+        }],
+        ['Quantum', {
+            name: 'Quantum',
+            imageUrl: 'assets/hsr/HSR_Quantum.jpg'
+        }],
+        ['Imaginary', {
+            name: 'Imaginary',
+            imageUrl: 'assets/hsr/HSR_Imaginary.jpg'
+        }],
+    ]),
+
+    ZZZElements: new Map([
+        ['Physical', {
+            name: 'Physical',
+            imageUrl: 'assets/zzz/ZZZ_Physical.jpg'
+        }],
+        ['Fire', {
+            name: 'Fire',
+            imageUrl: 'assets/zzz/ZZZ_Fire.jpg'
+        }],
+        ['Ice', {
+            name: 'Ice',
+            imageUrl: 'assets/zzz/ZZZ_Ice.jpg'
+        }],
+        ['Electric', {
+            name: 'Electric',
+            imageUrl: 'assets/zzz/ZZZ_Electric.jpg'
+        }],
+        ['Ether', {
+            name: 'Ether',
+            imageUrl: 'assets/zzz/ZZZ_Ether.jpg'
+        }],
+    ]),
+
+    HI3Elements: new Map([
+        ['Fire', {
+            name: 'Fire',
+            imageUrl: 'assets/hi3/HI3_Fire.png'
+        }],
+        ['Lightning', {
+            name: 'Lightning',
+            imageUrl: 'assets/hi3/HI3_Lightning.png'
+        }],
+        ['Ice', {
+            name: 'Ice',
+            imageUrl: 'assets/hi3/HI3_Ice.png'
+        }],
+        ['Physical', {
+            name: 'Physical',
+            imageUrl: 'assets/hi3/HI3_Physical.png'
+        }],
+    ]),
+}
