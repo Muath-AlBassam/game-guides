6fa9e9a7203c18d4b13322f19ae9e0037a306aed
Muath-AlBassam <muathalbassam@gmail.com>
2025-03-12 02:52:18 +0300

    move rotations to DB

--- src/repository/rotations.repository.js
+++ src/repository/rotations.repository.js
@@ -1,21 +1,61 @@
 
 class RotationsRepository {
 
+    constructor() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        dataClient.loadData('ROTATIONS').then(rotations => {
+            this.data = Utils.arrayTo2LevelMap(
+                rotations,
+                vArr => {
+                    return vArr.map(step => {
+                        let mappedStep = [];
+                        mappedStep.push(step.MEMBER);
+                        mappedStep.push(this.formatStepAction(step.ACTION, step.GAME_CODE));
+                        return mappedStep;
+                    })
+                },
+                'TEAM_CODE'
+            );
+        });
+    }
+
+    formatStepAction(action, gameCode) {
+        return action
+            .replace(/arrow/g, this.arrow)
+            .replace(/normal/g, gameCode == 'GI' ? this.gi.normal : 'TBD')
+            .replace(/charged/g, gameCode == 'GI' ? this.gi.charged : gameCode == 'ZZZ' ? this.zzz.charged : 'TBD')
+            .replace(/skill/g, gameCode == 'GI' ? this.gi.skill : 'TBD')
+            .replace(/ultimate/g, gameCode == 'GI' ? this.gi.ultimate : 'TBD')
+            .replace(/plunge/g, gameCode == 'GI' ? this.gi.plunge : 'TBD')
+            .replace(/basic/g, this.zzz.basic)
+            .replace(/exspecial/g, this.zzz.exSpecial)
+            .replace(/special/g, this.zzz.special)
+            .replace(/qte/g, this.zzz.QTE)
+            .replace(/dash/g, this.zzz.dash)
+            .replace(/assault/g, this.zzz.assault)
+            .replace(/burn/g, this.zzz.burn)
+            .replace(/shock/g, this.zzz.shock)
+            .replace(/ times /g, this.times)
+            .replace(/st_(.*?)_st/g, this.smallText("$1"))
+            .replace(/rp_(.*?)_rp/g, this.repeat("$1"))
+        }
+
     arrow = `<span style="margin: auto 5px;">${Constants.unicode.arrow}</span>`;
     times = Constants.unicode.times;
 
+    // st_***_st
     smallText = (text) => `<span style="font-size: 0.8rem; font-weight: normal; margin-left: 2px; vertical-align: 2px;">${text}</span>`;
-    tooltip = (text, tooltip) => `<span title="${tooltip}">${text}</span>`;
+    // rp_***_rp
     repeat = (text) => `<span class="arrow-border">${text}</span>`;
+
+    tooltip = (text, tooltip) => `<span title="${tooltip}">${text}</span>`;
     imageOf = (path, tooltip) => `<img src="${path}" width="30" title="${tooltip}" />`;
 
     // GI moves shortcuts
     gi = {
-        // normal: this.imageOf('assets/images/gi/icons/GI_NormalAttack.png', 'Normal Attack'),
-        // charged: this.tooltip('CA', 'Charged Attack'),
-        // skill: this.imageOf('assets/images/gi/icons/GI_Skill.png', 'Skill'),
-        // ultimate: this.imageOf('assets/images/gi/icons/GI_Ultimate.png', 'Ultimate'),
-        // plunge: this.imageOf('assets/images/gi/icons/GI_Plunge.png', 'Plunge'),
         normal: this.tooltip('N', 'Normal Attack'),
         charged: this.tooltip('CA', 'Charged Attack'),
         skill: this.tooltip('E', 'Skill'),
@@ -34,141 +74,16 @@ class RotationsRepository {
         assault: this.imageOf(Utils.getImageUrl('assets/images/zzz/icons/ZZZ_Physical.png'), 'Assault'),
         burn: this.imageOf(Utils.getImageUrl('assets/images/zzz/icons/ZZZ_Fire.png'), 'Burn'),
         shock: this.imageOf(Utils.getImageUrl('assets/images/zzz/icons/ZZZ_Electric.png'), 'Shock'),
+        freeze: this.imageOf(Utils.getImageUrl('assets/images/zzz/icons/ZZZ_Ice.png'), 'Freeze'),
+        corrupt: this.imageOf(Utils.getImageUrl('assets/images/zzz/icons/ZZZ_Ether.png'), 'Corrupt'),
     }
 
-    getAllRotations(gameCode) {
-        switch (gameCode) {
-            case Constants.games.GI:
-                return this.data.GIRotations;
-            case Constants.games.ZZZ:
-                return this.data.ZZZRotations;
-            case Constants.games.HI3:
-                return this.data.HI3Rotations;
-            default:
-                return new Map([]);
-        }
-    }
-    
     getRotations(gameCode, teamName) {
-        let rotations = this.getAllRotations(gameCode).get(teamName);
+        let rotations = this.data.get(gameCode)?.get(teamName);
         return rotations ?? []
     }
 
-    data = {
-        GIRotations: new Map([
-            ['Pyro', [
-                ['Mavuika', `${this.gi.skill} ${this.smallText('(Tap)')}`],
-                ['Xilonen', `${this.gi.skill} ${this.arrow} ${this.gi.normal}${this.times}2 ${this.arrow}`],
-                ['Citlali', `${this.gi.skill} ${this.arrow} ${this.gi.ultimate} ${this.arrow}`],
-                ['Bennett', `${this.gi.ultimate} ${this.arrow}`],
-                ['Mavuika', `${this.gi.ultimate} ${this.arrow} ${this.gi.charged} + ${this.gi.normal}`],
-            ]],
-            ['Hydro', [
-                ['Neuvillette', `${this.gi.normal}${this.times}1 ${this.arrow}`],
-                ['Kazuha', `${this.gi.skill} ${this.arrow}`],
-                ['Raiden Shogun', `${this.gi.skill} ${this.arrow}`],
-                ['Furina', `${this.gi.skill} ${this.arrow} ${this.gi.ultimate} ${this.arrow}`],
-                ['Neuvillette', `${this.gi.skill}/${this.gi.ultimate} ${this.arrow} ${this.gi.charged}`],
-            ]],
-            ['Dendro', [
-                ['Zhongli', `${this.gi.skill} ${this.smallText('(Hold)')} ${this.arrow}`],
-                ['Nahida', `${this.gi.ultimate} ${this.arrow} ${this.gi.skill} ${this.arrow}`],
-                ['Yae Miko', `${this.gi.skill}${this.times}3 ${this.arrow}`],
-                ['Tighnari', `${this.gi.ultimate} ${this.arrow} ${this.repeat(this.gi.charged)}`],
-            ]],
-            ['Electro', [
-                ['Raiden Shogun', `${this.gi.skill} ${this.arrow}`],
-                ['Chevreuse', `${this.gi.skill} ${this.smallText('(Hold)')} ${this.arrow} ${this.gi.ultimate} ${this.arrow}`],
-                ['Thoma', `${this.gi.ultimate} ${this.arrow} ${this.gi.skill} ${this.arrow}`],
-                ['Clorinde', `${this.repeat(`${this.gi.skill} + ${this.gi.normal}`)} ${this.arrow} ${this.gi.ultimate}`]
-            ]],
-            ['Anemo', [
-                ['Ororon', ` ${this.gi.ultimate} ${this.arrow} ${this.gi.skill} ${this.arrow}`],
-                ['Furina', `${this.gi.skill} ${this.arrow} ${this.gi.ultimate} ${this.arrow}`],
-                ['Bennett', `${this.gi.ultimate} ${this.arrow}`],
-                ['Chasca', `${this.gi.skill} ${this.arrow} ${this.gi.charged} ${this.smallText('(Hold)')}`]
-            ]],
-            ['Cryo', [
-                ['Wriothesley', `${this.gi.normal}${this.times}1 ${this.arrow}`],
-                ['Kazuha', `${this.gi.skill} ${this.arrow}`],
-                ['Kokomi', `${this.gi.skill} ${this.arrow}`],
-                ['Shenhe', `${this.gi.skill} ${this.smallText('(Hold)')} ${this.arrow} ${this.gi.ultimate} ${this.arrow}`],
-                ['Wriothesley', `${this.gi.skill} ${this.arrow} ${this.repeat(`${this.gi.normal} + ${this.gi.charged}`)}`],
-            ]],
-            ['Geo', [
-                ['Zhongli', `${this.gi.skill} ${this.smallText('(Hold)')} ${this.arrow}`],
-                ['Fischl', `${this.gi.skill} ${this.arrow}`],
-                ['Xingqiu', `${this.gi.ultimate} ${this.arrow}`],
-                ['Navia', `${this.gi.ultimate} ${this.arrow} ${this.gi.skill} + ${this.gi.normal}`],
-            ]],
-            ['Physical', [
-                ['Zhongli', `${this.gi.skill} ${this.smallText('(Hold)')} ${this.arrow}`],
-                ['Raiden Shogun', `${this.gi.skill} ${this.arrow}`],
-                ['Mika', `${this.gi.skill} ${this.arrow}`],
-                ['Eula', `${this.gi.normal} + ${this.gi.skill} + ${this.gi.ultimate}`],
-            ]]
-        ]),
-    
-        ZZZRotations: new Map([
-            ['Miyabi', [
-                ['Lucy', `${this.zzz.exSpecial} ${this.arrow} ${this.zzz.QTE} ${this.arrow}`],
-                ['Miyabi', `${this.repeat(`${this.zzz.exSpecial} + ${this.zzz.basic} ${this.smallText('(Until 6 charges)')} ${this.arrow} ${this.zzz.charged}`)}`],
-                ['Lycaon', `${this.zzz.QTE} ${this.smallText('(Optional)')}`],
-            ]],
-            ['Physical',  [
-                ['Caesar', `${this.zzz.exSpecial} ${this.arrow}`],
-                ['Seth', `${this.zzz.exSpecial} ${this.smallText('(Until 75%+ Resolve)')} ${this.arrow} ${this.zzz.charged} ${this.arrow} ${this.zzz.QTE} ${this.arrow}`],
-                ['Jane', `${this.zzz.basic} ${this.smallText('(Until Passion State)')} ${this.arrow} ${this.zzz.basic} + ${this.zzz.charged} + ${this.zzz.exSpecial}`]
-            ]],
-            ['Fire', [
-                ['Caesar', `${this.repeat(`${this.zzz.exSpecial} ${this.smallText('(Parry)')} + ${this.zzz.basic} + ${this.zzz.charged}`)} ${this.arrow} Stun ${this.arrow}`],
-                ['Burnice', `${this.repeat(`${this.zzz.charged} / ${this.zzz.exSpecial} ${this.smallText('(Hold)')}`)} ${this.arrow} ${this.zzz.burn}`],
-                ['Soldier 11', `(${this.zzz.exSpecial} ${this.arrow} ${this.zzz.basic}) / Timed ${this.zzz.basic}`],
-            ]],
-            ['Ice', [
-                ['Ellen', `${this.zzz.dash} + ${this.zzz.charged} ${this.arrow}`],
-                ['Lycaon', `${this.repeat(`${this.zzz.charged} + ${this.zzz.exSpecial}`)} ${this.arrow} Stun ${this.arrow} ${this.zzz.QTE} ${this.arrow}`],
-                ['Soukaku', `${this.zzz.special} ${this.smallText('(Hold)')} ${this.arrow} ${this.zzz.QTE} ${this.arrow}`],
-                ['Ellen', `${this.zzz.basic} + ${this.zzz.exSpecial}`],
-            ]],
-            ['Ether', [
-                ['Qingyi', `${this.repeat(`${this.zzz.basic} ${this.smallText('(Until 75%+ Voltage)')} ${this.arrow} ${this.zzz.charged}`)} ${this.arrow} Stun ${this.arrow} ${this.zzz.QTE} ${this.arrow}`],
-                ['Nicole', `${this.zzz.exSpecial} ${this.smallText('(Hold)')} ${this.arrow} ${this.zzz.basic}${this.times}1 ${this.arrow}`],
-                ['Zhu Yuan', `${this.zzz.charged} + ${this.zzz.exSpecial}`],
-            ]],
-            ['Disorder', [
-                ['Caesar', `${this.zzz.exSpecial} ${this.arrow}`],
-                ['Burnice', `${this.repeat(`${this.zzz.charged} / ${this.zzz.exSpecial} ${this.smallText('(Hold)')}`)} ${this.arrow} ${this.zzz.burn}`],
-                ['Yanagi', `${this.repeat(`(${this.zzz.basic} + ${this.zzz.special}) / ${this.zzz.exSpecial} ${this.smallText('(Hold)')}`)} ${this.arrow} ${this.zzz.shock}`],
-            ]],
-        ]),
-    
-        HI3Rotations: new Map([
-            ['Fire', [
-                ['Bronya (HoT)', `Skill ${this.arrow} QTE ${this.arrow}`],
-                ['Ai', `Skill ${this.arrow}`],
-                ['Bronya (HoT)', `Ult${this.times}2 ${this.arrow} QTE ${this.arrow}`],
-                ['Lantern', `Basic + Skill ${this.arrow} Charged ${this.arrow} Ult`],
-            ]],
-            ['Lightning', [
-                ['Bronya (HoT)', `Skill ${this.arrow} Ult${this.times}2 ${this.arrow} QTE ${this.arrow}`],
-                ['Eden', `Ult ${this.arrow} QTE ${this.arrow}`],
-                ['Lunar Vow', `Skill ${this.arrow} Ult`]
-            ]],
-            ['Ice', []],
-            ['Physical', [
-                ['Fu Hua (HoS)', `Basic ${this.smallText('(Spear combo)')} ${this.arrow} QTE ${this.arrow}`],
-                ['Carole', `CA ${this.arrow} Ult ${this.arrow} QTE ${this.arrow}`],
-                ['Durandal', `Basic + CA + Ult`]
-            ]],
-            ['Herrscher Trio',[
-                ['Bronya (HoT)', `Skill ${this.arrow} Ult${this.times}2 ${this.arrow} QTE ${this.arrow}`],
-                ['Kiana (HoFi)', `Basic ${this.arrow} CA ${this.arrow}`],
-                ['Mei (HoO)', `Basic ${this.arrow} CA ${this.arrow} Ult ${this.arrow} QTE ${this.arrow}`],
-                ['Kiana (HoFi)', `Skill ${this.arrow} Basic ${this.arrow} CA ${this.arrow} Evade ${this.smallText('(Hold)')} ${this.arrow} Basic ${this.arrow} Ult`],
-            ]]
-        ])
-    }
+    data = new Map([]);
 }
 
 const rotationsRepository = new RotationsRepository();
