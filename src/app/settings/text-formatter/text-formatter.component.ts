22dc808c0406fb0c9d68cd0fd5109e44be6ac4a2
Muath-AlBassam <muathalbassam@gmail.com>
2026-05-20 18:28:45 +0300

    enhance text formatter + update build

--- src/app/settings/text-formatter/text-formatter.component.ts
+++ src/app/settings/text-formatter/text-formatter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Constants } from '../../utils/constants';
 import { TextUtils } from '../../utils/text-utils';
@@ -14,18 +14,53 @@ export class TextFormatterComponent implements OnInit {
   games: any[] = [];
   gameCode: string = Constants.games.GI;
 
+  @ViewChild('myTextarea') textarea!: ElementRef<HTMLTextAreaElement>;
   text: string = '';
   formattedText: any = '';
-
   showFormatted: boolean = false;
+
+  formatsList: any[] = [];
 
   constructor(private textUtils: TextUtils, private gamesService: GamesService, @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {
     this.games = this.gamesService.getAll();
+    this.formatsList = this.textUtils.TEXT_FORMATS_LIST('X');
+    this.formatsList = this.formatsList.map(f => {
+      let rawText =  f.regex.toString().replace(' ', '').replace('(.*?)', '').replace('/', '').replace('/g', '');
+      f.rawText = rawText;
+      f.sample = rawText.substring(0, f.offset) + (f.offset > 0 && f.offset <= 3 ? 'Test' : '') + rawText.substring(f.offset);
+      f.sample = f.sample.replace(f.regex, f.replace);
+      return f;
+    });
   }
 
   formatText() {
     this.formattedText = this.textUtils.format(this.text, this.gameCode);
+  }
+
+  applyFormat(format: any) {
+    this.insertText(format.rawText, format.offset);
+    this.formatText();
+  }
+
+  getRawText(format: any) {
+    return format.regex.toString()
+      .replace(' ', '')
+      .replace('(.*?)', '')
+      .replace('/', '')
+      .replace('/g', '');
+  }
+
+  insertText(textToInsert: string, offset: number) {
+    const textarea = this.textarea.nativeElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    
+    this.text = this.text.substring(0, start) + textToInsert + this.text.substring(end);
+    textarea.value = this.text;
+    const newCursor = start + offset;
+    textarea.focus();
+    textarea.setSelectionRange(newCursor, newCursor);
   }
 }
--- src/app/utils/text-utils.ts
+++ src/app/utils/text-utils.ts
@@ -26,13 +26,14 @@ export class TextUtils {
   repeat = (text: string) => `<span class="arrow-border">${text}</span>`;
   tooltip = (text: string, tooltip: string) => `<span title="${tooltip}">${text}</span>`;
   htmlTooltip = (text: string, tooltip: string) => `<span class="html-tooltip">${text}<span class="tooltip-content">${tooltip}</span></span>`;
-  imageOf = (path: string, tooltip: string | null = null, style: string | null = null) => `<img src="${path}" width="30" title="${tooltip}" style="margin-top: -8px; ${style ?? ''}" />`;
+  imageOf = (path: string, tooltip: string | null = 'image', style: string | null = null) => `<img src="${path}" width="30" title="${tooltip}" style="margin-top: -8px; ${style ?? ''}" />`;
   color = (text: string, color: string) => `<b style="color: #${color}">${text}</b>`;
   bold = (text: string) => `<b>${text}</b>`;
 
   getCharacterImage(name: string, gameCode: string) {
-    let charmd = this.charactersService.getOne(gameCode, name?.trim());
-    return this.imageOf(charmd.imageUrl, name);
+    const charmd = this.charactersService.getOne(gameCode, name?.trim());
+    const imgUrl = charmd && charmd.imageUrl ? charmd.imageUrl : Constants.images.unknownCharacter;
+    return this.imageOf(imgUrl, name);
   }
 
   getCharacterImageAsTooltip(name: string, gameCode: string) {
@@ -73,70 +74,85 @@ export class TextUtils {
     corrupt: this.imageOf(Utils.appendRepoUrl('assets/images/zzz/icons/ZZZ_Ether.png'), 'Corrupt', 'margin-top: 0;'),
   }
 
+  TEXT_FORMATS_LIST(gameCode: string) {
+    return [
+      { title: 'Normal Attack', offset: 6, regex: /normal/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.normal : match },
+      { title: 'Charged Attack', offset: 7, regex: /charged/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.charged : this.isZZZ(gameCode) ? this.zzz.charged : match },
+      { title: 'Skill (Tap)', offset: 8, regex: /tapskill/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.tapSkill : match },
+      { title: 'Skill (Hold)', offset: 9, regex: /holdskill/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.holdSkill : match },
+      { title: 'Skill', offset: 5, regex: /skill/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.skill : match },
+      { title: 'Ultimate', offset: 8, regex: /ultimate/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.ultimate : this.isZZZ(gameCode) ? this.zzz.ultimate : match },
+      { title: 'Plunge', offset: 6, regex: /plunge/g, replace: (match: any) => this.isGI(gameCode) ? this.gi.plunge : match },
+      { title: 'Basic Attack', offset: 5, regex: /basic/g, replace: () => this.zzz.basic },
+      { title: 'EX Special', offset: 9, regex: /exspecial/g, replace: () => this.zzz.exSpecial },
+      { title: 'Special', offset: 7, regex: /special/g, replace: () => this.zzz.special },
+      { title: 'Chain Attack', offset: 5, regex: /chain/g, replace: () => this.zzz.chain },
+      { title: 'Dash', offset: 4, regex: /dash/g, replace: () => this.zzz.dash },
+      { title: 'Assault', offset: 7, regex: /assault/g, replace: () => this.zzz.assault },
+      { title: 'Burn', offset: 4, regex: /burn/g, replace: () => this.zzz.burn },
+      { title: 'Shock', offset: 5, regex: /shock/g, replace: () => this.zzz.shock },
+      { title: 'Switch', offset: 6, regex: /switch/g, replace: () => '>' },
+      { title: 'Multiply (x)', offset: 7, regex: / times /g, replace: () => Constants.unicode.times },
+      { title: 'Arrow', offset: 5, regex: /arrow/g, replace: () => `<span style="margin: auto 5px;">${Constants.unicode.arrow}</span>` },
+      { title: 'Character Image', offset: 2, regex: /c_(.*?)_c/g, replace: (match: any, capture: any) => this.getCharacterImage(capture, gameCode) },
+      { title: 'Character Image + Name', offset: 3, regex: /cn_(.*?)_cn/g, replace: (match: any, capture: any) => this.getCharacterImage(capture, gameCode) + ` <b class="no-break">${capture}</b>` },
+      { title: 'Character Tooltip', offset: 3, regex: /ca_(.*?)_ca/g, replace: (match: any, capture: any) => this.getCharacterImageAsTooltip(capture, gameCode) },
+      { title: 'Title', offset: 2, regex: /t_(.*?)_t/g, replace: (match: any, capture: any) => `<b style="text-transform: uppercase; margin-right: 10px;">${capture}:</b>` },
+      { title: 'Image', offset: 4, regex: /img_(.*?)_img/g, replace: (match: any, capture: any) => this.imageOf(Utils.appendRepoUrl(capture)) },
+      { title: 'Small Text', offset: 3, regex: /st_(.*?)_st/g, replace: (match: any, capture: any) => `<span style="font-size: 0.8rem; font-weight: normal; margin-left: 1px; vertical-align: 2px; opacity: 0.6;">${capture}</span>` },
+      { title: 'Bold', offset: 2, regex: /b_(.*?)_b/g, replace: (match: any, capture: any) => `<b>${capture}</b>` },
+      { title: 'Underline', offset: 2, regex: /u_(.*?)_u/g, replace: (match: any, capture: any) => `<u>${capture}</u>` },
+      { title: 'Italic', offset: 2, regex: /i_(.*?)_i/g, replace: (match: any, capture: any) => `<i>${capture}</i>` },
+      { title: 'New Line', offset: 3, regex: /nl_/g, replace: () => `<br/>` },
+      { title: 'Text + Tooltip', offset: 3, regex: /tp_(.*?)_(.*?)_tp/g, replace: (match: any, capture1: any, capture2: any) => this.htmlTooltip(capture1, capture2) },
+      { title: 'Combo', offset: 3, regex: /cb_(.*?)_cb/g, replace: (match: any, capture: any) => `<b>[</b>${capture}<b>]</b>` },
+      //{ title: 'TTT', offset: 333, regex: /REGEX/g, replace: () => {} },
+    ];
+  }
+
   format(text: string | null, gameCode: string) {
     if (text == null) {
       return '';
     }
-    // img_***_img => image
-    let formatted = String(text)
-      .replace(/normal/g, (match) => this.isGI(gameCode) ? this.gi.normal : match)
-      .replace(/charged/g, (match) => this.isGI(gameCode) ? this.gi.charged : this.isZZZ(gameCode) ? this.zzz.charged : match)
-      .replace(/tapskill/g, (match) => this.isGI(gameCode) ? this.gi.tapSkill : match)
-      .replace(/holdskill/g, (match) => this.isGI(gameCode) ? this.gi.holdSkill : match)
-      .replace(/skill/g, (match) => this.isGI(gameCode) ? this.gi.skill : match)
-      .replace(/ultimate/g, (match) => this.isGI(gameCode) ? this.gi.ultimate : this.isZZZ(gameCode) ? this.zzz.ultimate : match)
-      .replace(/plunge/g, (match) => this.isGI(gameCode) ? this.gi.plunge : match)
-      .replace(/basic/g, this.zzz.basic)
-      .replace(/exspecial/g, this.zzz.exSpecial)
-      .replace(/special/g, this.zzz.special)
-      .replace(/chain/g, this.zzz.chain)
-      .replace(/dash/g, this.zzz.dash)
-      .replace(/assault/g, this.zzz.assault)
-      .replace(/burn/g, this.zzz.burn)
-      .replace(/shock/g, this.zzz.shock)
-      .replace(/switch/g, '>')
-      .replace(/ times /g, Constants.unicode.times)
-      .replace(/arrow/g, `<span style="margin: auto 5px;">${Constants.unicode.arrow}</span>`)
-      .replace(/c_(.*?)_c/g, (match, capture) => this.getCharacterImage(capture, gameCode)) // character image
-      .replace(/cn_(.*?)_cn/g, (match, capture) => this.getCharacterImage(capture, gameCode) + ` <b class="no-break">${capture}</b>`) // character image + name
-      .replace(/ca_(.*?)_ca/g, (match, capture) => this.getCharacterImageAsTooltip(capture, gameCode)) // character name with image tooltip
-      .replace(/t_(.*?)_t/g, (match, capture) => `<b style="text-transform: uppercase; margin-right: 10px;">${capture}:</b>`) // title
-      .replace(/img_(.*?)_img/g, (match, capture) => this.imageOf(Utils.appendRepoUrl(capture))) // image
-      .replace(/st_(.*?)_st/g, (match, capture) => `<span style="font-size: 0.8rem; font-weight: normal; margin-left: 1px; vertical-align: 2px; opacity: 0.6;">${capture}</span>`) // small text
-      .replace(/b_(.*?)_b/g, (match, capture) => `<b>${capture}</b>`) // bold
-      .replace(/u_(.*?)_u/g, (match, capture) => `<u>${capture}</u>`) // underline
-      .replace(/i_(.*?)_i/g, (match, capture) => `<u>${capture}</u>`) // italic
-      .replace(/nl_/g, `<br/>`) // new line
-      .replace(/tp_(.*?)_(.*?)_tp/g, (match, capture1, capture2) => this.htmlTooltip(capture1, capture2)) // tooltip
-      .replace(/cb_(.*?)_cb/g, (match, capture) => `<b>[</b>${capture}<b>]</b>`) // new line;
-
+    let formatted = String(text);
+    for (const rule of this.TEXT_FORMATS_LIST(gameCode)) {
+      formatted = formatted.replace(rule.regex, rule.replace);
+    }
     return this.sanitizer.bypassSecurityTrustHtml(formatted);
+  }
+
+  COLOR_FORMATS_LIST(gameCode: string) {
+    return [
+      { title: 'Fire', offset: 8, regex: /fire dmg|burning/g, replace: (match: any) => this.isGI(gameCode) ? match : this.color(match, 'ff5521') },
+      { title: 'Electric', offset: 12, regex: /electric dmg|shocked/g, replace: (match: any) => this.isZZZ(gameCode) ? this.color(match, '2eb6ff') : match },
+      { title: 'Ether', offset: 9, regex: /ether dmg|corruption/g, replace: (match: any) => this.color(match, 'fe437e') },
+      { title: 'Ice', offset: 7, regex: /ice dmg|freeze|shatter/g, replace: (match: any) => this.color(match, '98eff0') },
+      { title: 'Physical', offset: 12, regex: /physical dmg/g, replace: (match: any) => this.isHSR(gameCode) ? this.color(match, '979797') : this.isZZZ(gameCode) ? this.color(match, 'f0d12b') : match },
+      { title: 'Lightning', offset: 13, regex: /lightning dmg|electro dmg/g, replace: (match: any) => this.color(match, 'c65ade') },
+      { title: 'Wind', offset: 8, regex: /wind dmg/g, replace: (match: any) => this.color(match, '61cf93') },
+      { title: 'Quantum', offset: 11, regex: /quantum dmg/g, replace: (match: any) => this.color(match, '766dd6') },
+      { title: 'Imaginary', offset: 13, regex: /imaginary dmg/g, replace: (match: any) => this.color(match, 'f3e137') },
+      { title: 'Pyro', offset: 8, regex: /pyro dmg/g, replace: (match: any) => this.color(match, 'ef7938') },
+      { title: 'Cryo', offset: 8, regex: /cryo dmg/g, replace: (match: any) => this.color(match, '9fd6e3') },
+      { title: 'Hydro', offset: 9, regex: /hydro dmg/g, replace: (match: any) => this.color(match, '4cc2f1') },
+      { title: 'Electro', offset: 11, regex: /electro dmg/g, replace: (match: any) => this.color(match, 'af8ec1') },
+      { title: 'Anemo', offset: 9, regex: /anemo dmg/g, replace: (match: any) => this.color(match, '74c2a8') },
+      { title: 'Geo', offset: 7, regex: /geo dmg/g, replace: (match: any) => this.color(match, 'fab632') },
+      { title: 'Dendro', offset: 10, regex: /dendro dmg/g, replace: (match: any) => this.color(match, 'a5c83b') },
+      { title: 'Numbers (%)', offset: 0, regex: /\d+(\.\d+)?%/g, replace: (match: any) => this.color(match, 'f3e137') },
+      { title: 'Numbers (s)', offset: 0, regex: /\d+(\.\d+)?s/g, replace: (match: any) => this.color(match, 'f3e137') },
+      //{ title: 'TTT', offset: 333, regex: /REGEX/g, replace: () => {} },
+    ];
   }
 
   colorize(text: string | null, gameCode: string) {
     if (text == null) {
       return '';
     }
-    let colorized = String(text)
-      .replace(/fire dmg|burning/gi, (match) => this.isGI(gameCode) ? match : this.color(match, 'ff5521'))
-      .replace(/electric dmg|shocked/gi, (match) => this.isZZZ(gameCode) ? this.color(match, '2eb6ff') : match)
-      .replace(/ether dmg|corruption/gi, (match) => this.color(match, 'fe437e'))
-      .replace(/ice dmg|freeze|shatter/gi, (match) => this.color(match, '98eff0'))
-      .replace(/physical dmg/gi, (match) => this.isHSR(gameCode) ? this.color(match, '979797') : this.isZZZ(gameCode) ? this.color(match, 'f0d12b') : match)
-      .replace(/lightning dmg|electro dmg/gi, (match) => this.color(match, 'c65ade'))
-      .replace(/wind dmg/gi, (match) => this.color(match, '61cf93'))
-      .replace(/quantum dmg/gi, (match) => this.color(match, '766dd6'))
-      .replace(/imaginary dmg/gi, (match) => this.color(match, 'f3e137'))
-      .replace(/pyro dmg/gi, (match) => this.color(match, 'ef7938'))
-      .replace(/cryo dmg/gi, (match) => this.color(match, '9fd6e3'))
-      .replace(/hydro dmg/gi, (match) => this.color(match, '4cc2f1'))
-      .replace(/electro dmg/gi, (match) => this.color(match, 'af8ec1'))
-      .replace(/anemo dmg/gi, (match) => this.color(match, '74c2a8'))
-      .replace(/geo dmg/gi, (match) => this.color(match, 'fab632'))
-      .replace(/dendro dmg/gi, (match) => this.color(match, 'a5c83b'))
-      .replace(/\d+(\.\d+)?%/g, (match) => this.color(match, 'f3e137')) // numbers (%)
-      .replace(/\d+(\.\d+)?s/g, (match) => this.color(match, 'f3e137')) // numbers (s)
-
+    let colorized = String(text);
+    for (const rule of this.COLOR_FORMATS_LIST(gameCode)) {
+      colorized = colorized.replace(rule.regex, rule.replace);
+    }
     return this.sanitizer.bypassSecurityTrustHtml(colorized);
   }
 }
